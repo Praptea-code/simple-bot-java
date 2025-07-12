90b48ed062a8913a9c093e4c56410994247d9ae5
Praptea-code <bhattarai.prapti00@gmail.com>
2025-07-12 08:59:26 +0545

    added the greetings

--- SimpleBot.java
+++ SimpleBot.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;//we will be taking inputs
 public class SimpleBot {
-    
+    Scanner scanner = new Scanner(System.in);//scanner object to take inputs from user
+    public static void main (String[] args) {
+        
+}
+public void greet(String botName,int botBirthdDate){
+    System.out.println("Hello, I am " + botName + "\nI was born on " + botBirthdDate + ".");
+    System.out.println("Please let me know your name,hehe!");
 }
