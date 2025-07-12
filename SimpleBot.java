3d70d420f02785513792e4ffdec480fbcd397bbd
Praptea-code <bhattarai.prapti00@gmail.com>
2025-07-12 09:54:04 +0545

    fix:change delay times and add comments about delay

--- SimpleBot.java
+++ SimpleBot.java
@@ -16,7 +16,7 @@ public class SimpleBot {
     public void greet(String botName, int botBirthdDate) {
         System.out.println("Hello, I am " + botName +" (●'◡'●)."+ "\nI was born on " + botBirthdDate + ".");
         try{
-            Thread.sleep(2000);
+            Thread.sleep(2000);//added delays for better user experience
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
@@ -43,13 +43,13 @@ public class SimpleBot {
     //this method is to guess age using a simple trick
     public void yourAge() {
         try{
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
         System.out.println("Let me guess your age now!");
         try{
-            Thread.sleep(3000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
@@ -71,7 +71,7 @@ public class SimpleBot {
         }
         System.out.println("Aha! you're " + age + " years old!");
         try{
-            Thread.sleep(1000);
+            Thread.sleep(800);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
@@ -81,19 +81,19 @@ public class SimpleBot {
     //take user input for number and count to it
     public void count(){
         try{
-            Thread.sleep(3000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
         System.out.println("You know i have been taught to count as well");
         try{
-            Thread.sleep(2000);
+            Thread.sleep(500);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
         System.out.println("Let me prove it to you");
         try{
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
@@ -102,13 +102,13 @@ public class SimpleBot {
         for(int i=1;i<=number;i++){//used for loop for convenience
             System.out.println(i);
             try{
-                Thread.sleep(1000);
+                Thread.sleep(700);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
             }
         try{
-            Thread.sleep(2000);
+            Thread.sleep(200);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
@@ -118,17 +118,16 @@ public class SimpleBot {
     //ended the conversation
     public void end(){
         try{
-            Thread.sleep(3000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
         System.out.println("Well, this is all i can do for now twin");
         try{
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
         System.out.println("I hope you had fun !!");
     }
 }
-//
